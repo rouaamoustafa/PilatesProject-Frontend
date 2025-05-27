0c6e957d9269bbbc5fd51ba419804352c61d616d
RouaaM <moustafarouaa@gmail.com>
2025-05-27 05:00:07 +0300

    slice

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     //process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api'
-    baseUrl: '/api' ,
+    baseUrl: process.env.NEXT_PUBLIC_API_URL ,
     prepareHeaders: (headers) => {
       if (typeof window !== 'undefined') {
         const token = localStorage.getItem('auth_token');
