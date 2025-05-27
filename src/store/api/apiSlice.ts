700c2efd3e50c91f1c4092c5fbdc1d85c2a8edf7
RouaaM <moustafarouaa@gmail.com>
2025-05-27 10:48:44 +0300

    slice 3

--- src/store/api/apiSlice.ts
+++ src/store/api/apiSlice.ts
@@ -1,6 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
+  reducerPath:'api',
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
     prepareHeaders: (headers) => {
